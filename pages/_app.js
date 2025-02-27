31d709ae455aa809f2540036d05c362768ef4260
EpicZuko <zukokalilov5@gmail.com>
2025-02-27 23:04:36 +0600

    master

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import Layout from "./layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const isCrmPage = pageProps?.path?.startsWith('/crm');
+  return <>
+  {isCrmPage ? (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  ) : (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )}
+</>
 }
